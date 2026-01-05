31391aab82ec8d7f98fac9f00941581f6c4273cd
Michel Sánchez Montells <michel.sanchez_montells@wassermeloni.de>
2026-01-05 15:42:21 +0100

    Using array and Assinging command output to a variable

--- analyse-logs.sh
+++ analyse-logs.sh
@@ -1,10 +1,32 @@
-find . -name "*.log" -mtime -1
+#!/bin/bash
 
-log_folder='logs'
-grep "ERROR" $log_folder/application.log
-grep -c "ERROR" $log_folder/application.log
-grep -c "FATAL" $log_folder/application.log
+echo "Analizing logs files"
+echo "===================="
 
-grep -c "FATAL" $log_folder/system.log
-grep -c "CRITICAL" $log_folder/system.log
-grep "CRITICAL" $log_folder/system.log
+LOG_DIR="/home/montells/projects/bash_scripting/logs"
+APP_LOG_FILE="application.log"
+SYS_LOG_FILE="system.log"
+
+ERROR_PATTERNS=("ERROR" "FATAL" "CRITICAL")
+
+echo -e "\nList of logs files updated in the last 24 hours"
+LOGS_FILES=$(find $LOG_DIR -name "*.log" -mtime -1)
+echo "$LOGS_FILES"
+
+echo -e "\nSearching ERROR logs in applicaton.log file"
+grep "${ERROR_PATTERNS[0]}" "$LOG_DIR/$APP_LOG_FILE"
+
+echo -e "\nNumber of ERROR logs found in application.log"
+grep -c "${ERROR_PATTERNS[0]}" "$LOG_DIR/$APP_LOG_FILE"
+
+echo -e "\nNumber of FATAL logs found in application.log"
+grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$APP_LOG_FILE"
+
+echo -e "\nNumber of FATAL logs found in system.log"
+grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$SYS_LOG_FILE"
+
+echo -e "\nNumber of CRITICAL logs found in system.log"
+grep -c "${ERROR_PATTERNS[2]}" "$LOG_DIR/$SYS_LOG_FILE"
+
+echo -e "\nCRITICAL logs in system.log file"
+grep "${ERROR_PATTERNS[2]}" "$LOG_DIR/$SYS_LOG_FILE"
